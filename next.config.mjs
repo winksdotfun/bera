47ab69946477faaae39e99a5d11f379b2f7a1943
thirumurugan7 <thiru82003@gmail.com>
2025-04-01 10:20:33 +0530

    update Next.js configuration to set output directory to 'dist' and adjust TypeScript configuration to include 'dist/types/**/*.ts'

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'dist', // This will output to /workspace/dist
   images: {
     unoptimized: true,
     domains: [
